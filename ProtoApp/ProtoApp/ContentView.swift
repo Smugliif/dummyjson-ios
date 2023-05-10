b2dd94470ffb8e138dd89933c114739847363811
Smugliif <valtteri.havula123@gmail.com>
2023-05-10 23:12:11 +0300

    feat: Users displayed as a List of NavigationLinks taking to user specific views

---
+++ ProtoApp/ProtoApp/ContentView.swift
@@ -0,0 +1,140 @@
+//
+//  ContentView.swift
+//  ProtoApp
+//
+//  Created by Valtteri Havula on 8.5.2023.
+//
+
+//TODO:
+// Error handling in case fetch fails
+
+import SwiftUI
+import Alamofire
+
+
+
+struct HttpResults: Codable {
+    let users: [User]
+}
+
+struct User: Codable {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let age: Int
+    let gender: String
+    let image: String
+    let phone: String
+}
+
+struct UserView: View {
+    let user: User
+    var body: some View {
+        VStack {
+            Text("\(user.firstName) \(user.lastName)").font(.largeTitle)
+            Text("\(user.phone)")
+            Spacer()
+            Text("\(user.age) years old")
+            Spacer()
+        }
+        
+    }
+}
+
+struct NewUser: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+struct NewUserView: View {
+    @State var firstName: String = ""
+    @State var lastName: String = ""
+    
+    func addUser() {
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        let parameters: [String: Any] = ["firstName": firstName, "lastName": lastName]
+        if (!firstName.isEmpty && !lastName.isEmpty) {
+            AF.request("https://dummyjson.com/users/add",
+                       method: .post,
+                       parameters: parameters,
+                       encoding: JSONEncoding.default, headers: headers)
+            .responseDecodable(of: NewUser.self) { response in
+                switch response.result {
+                case .success(let user):
+                    print("Added user: \(user)")
+                case let .failure(error):
+                    print("Failed to add user: \(error)")
+                }
+            }
+        } else {
+            print("Names must be provided")
+        }
+    }
+    
+    var body: some View {
+        Text("Add new users").font(.largeTitle).bold()
+        Spacer()
+        VStack {
+            TextField("First Name", text: $firstName)
+            TextField("Last Name", text: $lastName)
+            HStack {
+                Button(action: addUser) {
+                    Label("Add User", systemImage: "plus")
+                }
+            }
+        }
+        .textFieldStyle(.roundedBorder)
+        Spacer()
+    }
+}
+
+struct ContentView: View {
+    @State var people: Array<User>? = nil
+    
+    func fetchUsers() {
+        AF.request("https://dummyjson.com/users")
+            .responseDecodable(of: HttpResults.self) { response in
+                if let result = response.value {
+                    self.people = result.users
+                } else {return}
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Users").bold().font(.largeTitle)
+                if let users = people {
+                    NavigationStack {
+                        List(users, id: \.id) { user in
+                            NavigationLink(destination: UserView(user: user)) {
+                                Text("\(user.firstName) \(user.lastName)")
+                            }
+                        }
+                    }
+                } else {
+                    ProgressView()
+                }
+            }
+            .onAppear { fetchUsers() }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Search")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: NewUserView()) {
+                        Text("Add new user")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProtoApp/ProtoApp/ProtoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtoAppApp.swift
+//  ProtoApp
+//
+//  Created by Valtteri Havula on 8.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProtoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
